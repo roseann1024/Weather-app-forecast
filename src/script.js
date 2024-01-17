9196483bae0a549fa9c074e1f9849396894b01c8
roseann1024 <156710202+roseann1024@users.noreply.github.com>
2024-01-17 15:20:02 -0700

    Added html/css/js - weather app

---
+++ src/script.js
@@ -0,0 +1,57 @@
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-app-icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+function searchCity(city) {
+  let apiKey = "f78a0dbafabf13190c441b8cod34ftff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function searchButton(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-input-city");
+
+  searchCity(searchInput.value);
+}
+
+let formInput = document.querySelector("#form-input");
+formInput.addEventListener("submit", searchButton);
+
+searchCity("Centennial");
